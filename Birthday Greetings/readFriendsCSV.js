2aed6ac27083a7bdd4b28fc64f5fa88ddb262fe8
Youness <107858907+YounessYS@users.noreply.github.com>
2023-01-13 09:52:02 +0000

    Birthday Greetings v1.1

---
+++ Birthday Greetings/readFriendsCSV.js
@@ -0,0 +1,96 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+const todayDate = new Date();
+let detailedDate = todayDate.toISOString().substring(0, 10);
+detailedDate = detailedDate.split("");
+detailedDate[4] = "/";
+detailedDate[7] = "/";
+detailedDate = detailedDate.join("");
+
+const febTwentyNinth = "02/29";
+const febTwentyEigth = "02/28";
+
+function readFromCSV() {
+  return fs
+    .createReadStream("./friends.csv", { encoding: "utf8" })
+    .pipe(parse({ delimiter: ",", from_line: 2 }))
+    .on("data", function (row) {
+      console.log(row);
+    })
+    .on("end", function () {
+      console.log("You've run out of friends");
+    })
+    .on("error", function (error) {
+      console.log(error.message);
+    });
+}
+
+function sendEmail() {
+  return fs
+    .createReadStream("./friends.csv", { encoding: "utf8" })
+    .pipe(parse({ delimiter: ",", from_line: 2 }))
+    .on("data", function (row) {
+      // If today is their birthday, email them while taking into account 29th Feb
+      if (
+        " " + detailedDate.indexOf(febTwentyEigth) > -1 &&
+        row[2].indexOf(febTwentyNinth) > -1
+      ) {
+        console.log(
+          "Happy birthday!",
+          "\n",
+          "Happy birthday, dear" + row[0] + "!"
+        );
+      } else if (
+        " " + detailedDate === row[2] &&
+        " " + detailedDate.indexOf(febTwentyNinth) == -1
+      ) {
+        console.log(
+          "Happy birthday!",
+          "\n",
+          "Happy birthday, dear" + row[0] + "!"
+        );
+      } else {
+        console.log("No birthdays today");
+      }
+    })
+    .on("end", function () {
+      console.log("You've run out of friends to email");
+    })
+    .on("error", function (error) {
+      console.log(error.message);
+    });
+}
+
+function sendSMS() {
+  return fs
+    .createReadStream("./friends.csv", { encoding: "utf8" })
+    .pipe(parse({ delimiter: ",", from_line: 2 }))
+    .on("data", function (row) {
+      // If today is their birthday, text them while taking into account 29th Feb
+      if (
+        " " + detailedDate.indexOf(febTwentyEigth) > -1 &&
+        row[2].indexOf(febTwentyNinth) > -1
+      ) {
+        console.log("Happy birthday, dear" + row[0] + "!");
+      } else if (
+        " " + detailedDate === row[2] &&
+        " " + detailedDate.indexOf(febTwentyNinth) == -1
+      ) {
+        console.log("Happy birthday, dear" + row[0] + "!");
+      } else {
+        console.log("No birthdays today");
+      }
+    })
+    .on("end", function () {
+      console.log("You've run out of friends to text");
+    })
+    .on("error", function (error) {
+      console.log(error.message);
+    });
+}
+
+console.log(detailedDate);
+readFromCSV();
+sendEmail();
+sendSMS();
---
+++ Birthday Greetings/sqlData.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+const sqlDB = require("./sqlDB");
+
+fs.createReadStream("./friends.csv", { encoding: "utf8" })
+  .pipe(parse({ delimiter: ",", from_line: 2 }))
+  .on("data", function (row) {
+    sqlDB.serialize(function () {
+      sqlDB.run(
+        `INSERT INTO my_friends VALUES (?, ?, ?, ?)`,
+        [row[0], row[1], row[2], row[3]],
+        function (error) {
+          if (error) {
+            return console.log(error.message);
+          }
+          console.log(`Row id: ${this.lastID}`);
+        }
+      );
+    });
+  });
